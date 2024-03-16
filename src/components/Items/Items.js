a11a956cc90c07b47866275e9e2c194d9aff2588
Erfan Habibi <erfan.habibi6.79@gmail.com>
2024-03-16 23:44:19 +0330

    delete class name in index

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -13,7 +13,7 @@ export default function Items() {
       </div>
       <div className='bg-blackCus-100 mt-[360px] sm:mt-[550px] lg:mt-[770px] pb-10 relative'>
         <div id='header' className='mb-10 relative container'>
-        <h1 className="text-4xl lg:text-6xl font-MorabbaBold text-white -top-28 lg:-top-[400px] relative">کافه آروماتیکا</h1>
+        <h1 className="text-4xl lg:text-6xl font-MorabbaBold text-white -top-24 lg:-top-[400px] relative">کافه آروماتیکا</h1>
           <h4 className='text-gold-100 font-MorabbaMedium text-lg lg:text-center'>بهترین ها را انتخاب کنید</h4>
           <h2 className='text-white text-3xl font-MorabbaMedium py-10 pt-0 lg:text-center'>منو قهوه آروماتیکا</h2>
           <div className='bg-black w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
